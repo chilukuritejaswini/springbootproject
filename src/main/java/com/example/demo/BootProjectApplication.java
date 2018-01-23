a97dc9d95aa12c04c8bcff9ad65d3c8940974230
tejaswini <tejaswini.chilukuri@veraction.com>
2018-01-23 19:34:14 +0530

    'springbootproject'

---
+++ src/main/java/com/example/demo/BootProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProjectApplication.class, args);
+	}
+}
